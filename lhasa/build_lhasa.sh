fdfa8718fe619ae09ccddfec31bca13815df36bf
Jakub Smulski <hgonomeg@gmail.com>
2024-09-02 18:04:59 +0200

    Add gemmi to the build system, build 64-bit Lhasa by default

--- lhasa/build_lhasa.sh
+++ lhasa/build_lhasa.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/sh
 source ./EMSCRIPTEN_CONFIG
-INSTALL_DIR=${PWD}/prefix
+if test x"${MEMORY64}" = x"1"; then
+    echo Build Lhasa: Using 64-bit prefix: ${PWD}/prefix64
+    echo
+    INSTALL_DIR=${PWD}/prefix64
+else
+    echo Build Lhasa: Using 32-bit prefix: ${PWD}/prefix
+    echo
+    INSTALL_DIR=${PWD}/prefix
+fi
 emcmake cmake -DMEMORY64=${MEMORY64} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -S . -B lhbuild &&\
 cd lhbuild/;\
 emmake make LDFLAGS=-all-static -j16;\

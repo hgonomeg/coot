9aa01ca638fe94701f9c144e7ab6b054b13275fe
Jakub Smulski <hgonomeg@gmail.com>
2024-09-15 08:07:30 +0200

    Declare custom coordgen wrapper

--- layla/coordgen.cpp
+++ layla/coordgen.cpp
@@ -1,1 +1,5 @@
 #include "coordgen.hpp"
+
+unsigned int coot::layla::addCoordgenConformer(::RDKit::RWMol& mol,  const coot::layla::CoordgenOptions* const opts) {
+    return -1;
+}
--- layla/coordgen.hpp
+++ layla/coordgen.hpp
@@ -1,6 +1,16 @@
 #ifndef LAYLA_COORDGEN_HPP
 #define LAYLA_COORDGEN_HPP
+#include <rdkit/GraphMol/RWMol.h>
+namespace coot::layla {
 
+struct CoordgenOptions {
+
+};
+
+/// Todo: doc
+unsigned int addCoordgenConformer(::RDKit::RWMol& mol, const CoordgenOptions* const opts = nullptr);
+
+} // namespace coot::layla
 
 
 #endif

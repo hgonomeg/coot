e9c3983f88844b77e282b39fb5e261badf04aa69
Jakub Smulski <hgonomeg@gmail.com>
2024-09-17 09:48:56 +0200

    Finish a bare-minimum implementation of 'addCoordgenConformer'

--- layla/coordgen.cpp
+++ layla/coordgen.cpp
@@ -1,6 +1,14 @@
 #include "coordgen.hpp"
+#include <rdkit/GraphMol/RDKitBase.h>
+#include <rdkit/GraphMol/MolTransforms/MolTransforms.h>
+#include <rdkit/Geometry/Transform3D.h>
 #include <coordgen/sketcherMinimizer.h>
 #include <memory>
+#ifdef __EMSCRIPTEN__
+#include "../lhasa/glog_replacement.hpp"
+#else
+#include <glib.h>
+#endif
 
 coot::layla::CoordgenOptions::CoordgenOptions() 
     :coordMap(nullptr),
@@ -24,10 +32,88 @@ unsigned int coot::layla::addCoordgenConformer(::RDKit::RWMol& mol,  const coot:
     // Does it leak memory?
     auto* minimizer_mol = new sketcherMinimizerMolecule();
 
+    std::vector<sketcherMinimizerAtom*> atoms(mol.getNumAtoms());
+    for (auto atit = mol.beginAtoms(); atit != mol.endAtoms(); ++atit) {
+        RDKit::Atom* rdatom = *atit;
+        sketcherMinimizerAtom* atom = minimizer_mol->addNewAtom();
+        atom->molecule = minimizer_mol;
+        atom->atomicNumber = rdatom->getAtomicNum();
+        atom->charge = rdatom->getFormalCharge();
+        if(opts->coordMap) {
+            auto coord_it = opts->coordMap->find(rdatom->getIdx());
+            if(coord_it != opts->coordMap->end()) {
+                const RDGeom::Point2D& coords = coord_it->second;
+                atom->fixed = true;
+                atom->constrained = true;
+                // or just `atom->coordinates` ?
+                atom->templateCoordinates = sketcherMinimizerPointF(
+                    coords.x * opts->coordgenScaling, coords.y * opts->coordgenScaling
+                );
+            }
+        }
+        atoms[rdatom->getIdx()] = atom;
+    }
+
+    std::vector<sketcherMinimizerBond*> bonds(mol.getNumBonds());
+    for (auto bondit = mol.beginBonds(); bondit != mol.endBonds(); ++bondit) {
+        RDKit::Bond* rdbond = *bondit;
+        sketcherMinimizerBond* bond = minimizer_mol->addNewBond(
+            atoms[rdbond->getBeginAtomIdx()],
+            atoms[rdbond->getEndAtomIdx()]
+        );
+        // FIX: This is no doubt wrong
+        switch (rdbond->getBondType()) {
+            case RDKit::Bond::ZERO:
+                bond->bondOrder = 0;
+                break;
+            case RDKit::Bond::SINGLE:
+                bond->bondOrder = 1;
+                break;
+            case RDKit::Bond::DOUBLE:
+                bond->bondOrder = 2;
+                break;
+            case RDKit::Bond::TRIPLE:
+                bond->bondOrder = 3;
+                break;
+            case RDKit::Bond::AROMATIC:
+                bond->bondOrder = 1;
+                break;
+            default:
+                g_warning("addCoordgenConformer: Unknown bond type: %i", rdbond->getBondType());
+        }
+        bonds[rdbond->getIdx()] = bond;
+    }
+
+    minimizer_mol->assignBondsAndNeighbors(atoms, bonds);
+    // todo: stereo bonds
+
+    minimizer.initialize(minimizer_mol);
+    minimizer.runGenerateCoordinates();
+
     // Does it leak memory?
     auto conf = new RDKit::Conformer(mol.getNumAtoms());
     conf->set3D(false);
+
+    for (unsigned int i = 0; i < mol.getNumAtoms(); ++i) {
+        auto coords = RDGeom::Point3D(
+            atoms[i]->coordinates.x() / opts->coordgenScaling,
+            atoms[i]->coordinates.y() / opts->coordgenScaling, 
+            0.0
+        );
+        conf->setAtomPos(i, coords);
+    }
+
     mol.clearConformers();
     auto res = mol.addConformer(conf, true);
+    if (!opts->coordMap || opts->coordMap->empty()) {
+        //RDGeom::Point3D centroid{0.0, 0.0, 0.0};
+        // center the coordinates
+        RDGeom::Transform3D tf;
+        auto centroid = MolTransforms::computeCentroid(*conf);
+        centroid *= -1;
+        tf.SetTranslation(centroid);
+        MolTransforms::transformConformer(*conf, tf);
+    }
+    
     return res;
 }
